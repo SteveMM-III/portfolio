08a8ecfc8bca0765020a5ce220728bbe9bdfdf0d
Kronykus <MoreheadSteve@gmail.com>
2020-01-25 08:58:24 -0600

    formatting

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
-import Home from './sections/Home';
-import Intro from './sections/Intro';
-import Projects from './sections/Projects';
-import About from './sections/About';
-import Contact from './sections/Contact';
+import Home       from './sections/Home';
+import Intro      from './sections/Intro';
+import Projects   from './sections/Projects';
+import About      from './sections/About';
+import Contact    from './sections/Contact';
 import Navigation from './Navigation';
 
 const Body = () => {
@@ -14,11 +14,11 @@ const Body = () => {
   return (
     <main>
       <div className='fullWidth w3-display-container'>
-        { current === 'home' && <Home /> }
-        { current === 'intro' && <Intro /> }
+        { current === 'home'     && <Home     /> }
+        { current === 'intro'    && <Intro    /> }
         { current === 'projects' && <Projects /> }
-        { current === 'about' && <About /> }
-        { current === 'contact' && <Contact /> }
+        { current === 'about'    && <About    /> }
+        { current === 'contact'  && <Contact  /> }
         <Navigation setCurrent={ setCurrent } />
       </div>
     </main>
